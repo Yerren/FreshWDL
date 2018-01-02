fc561112a5195b02e0a5e0cd7c91ac8086d1b4ab
Yerren <yerren@renerica.com>
2018-01-02 16:33:32 +1300

    Changed HTML content to be dynamically loaded from server, instead of hard coded.
    Additionally, combined majority of JS scripts into one file: WidgetsHandlers.js

---
+++ UpperContent.js
@@ -0,0 +1,68 @@
+document.write('\
+    <!-- The Records Modal -->\
+    <div id="recordsModal" class="modal">\
+\
+        <!-- Modal content -->\
+        <div class="modal-content">\
+            <div id="recordsHeader" class="modal-header">\
+                <span id="recordsClose" class="close">&#215</span>\
+                <h2 id="recordsHeaderText" class="header-text">Records</h2>\
+            </div>\
+            <div id="modalRecordsDiv">\
+                <table id="recordsTable">\
+                </table>\
+            </div>\
+            <div id="recordsFooter" class="modal-footer">\
+                <h3 class="footer-text"></h3>\
+                <div class="select-style">\
+                    <select id="selectMenuRecords" onchange="changeCurrentOptionRe01(this.value);">\
+                        <option value=0>Records for this month</option>\
+                        <option value=1>Records for this year</option>\
+                        <option value=2>All time records</option>\
+                    </select>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+\
+    <!-- The Forecast Modal -->\
+    <div id="forecastModal" class="modal">\
+\
+        <!-- Modal content -->\
+        <div class="modal-content">\
+            <div id="forecastHeader" class="modal-header">\
+                <span id="forecastClose" class="close">&#215</span>\
+                <h2 id="forecastHeaderText" class="header-text">Forecast</h2>\
+            </div>\
+            <div id="modalForecastDiv">\
+            </div>\
+            <div id="forecastFooter" class="modal-footer">\
+                <h3 class="footer-text"></h3>\
+            </div>\
+        </div>\
+    </div>\
+\
+    <!-- The Graph Modal -->\
+    <div id="myModal" class="modal">\
+\
+        <!-- Modal content -->\
+        <div class="modal-content">\
+            <div id="graphHeader" class="modal-header">\
+                <span id="graphClose" class="close">&#215</span>\
+                <h2 id="graphHeaderText" class="header-text">Modal Header</h2>\
+            </div>\
+            <div id="ModalCanvasDiv" class="OuterCanvasDiv">\
+                <canvas id="ModalCanvas" style="background-color:#FFFFFF">\
+                Your browser does not support the canvas element.\
+                </canvas>\
+            </div>\
+            <div id="graphFooter" class="modal-footer">\
+                <h3 class="footer-text"></h3>\
+                <div class="select-style">\
+                    <select id="selectMenu" onchange="graphChange(this);">\
+                    </select>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+');
---
+++ js_bundles/Loading.js
@@ -0,0 +1,60 @@
+/*jslint plusplus: true, sloppy: true, indent: 4 */
+var op = 1,
+    dataCollectErrorCR = false,
+    dataCollectErrorCRE = false,
+    dataCollectErrorCRH = false,
+    dataCollectErrorCRD = false;
+
+function fade() {
+    var element = document.getElementById("loadingScreen");
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+            clearInterval(loadingTimer); // stop the dots
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+function unfade(element) {
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+//Add "." to end of loading message, up to three, then remove two
+var count = 1;
+function addDots() {
+    var element = document.getElementById("loadingMessage"),
+        dotIndex = element.innerHTML.indexOf("."),
+        stringOut = element.innerHTML.substring(0, dotIndex);
+    
+    //check if there is an error in collecting data
+    if (dataCollectErrorCR === true || dataCollectErrorCRE === true || dataCollectErrorCRH === true || dataCollectErrorCRD === true) {
+        stringOut = "Data Currently Unavailable";
+    }
+    
+    count += 1;
+    if (count > 3) {
+        count = 1;
+    }
+    stringOut = stringOut + "...";
+    stringOut = stringOut.substring(0, stringOut.length + count - 3);
+    element.innerHTML = stringOut;
+}
+
+function loadingFinished() {
+    fade();
+}
+
+//Start dots
+var loadingTimer = setInterval(addDots, 500);
