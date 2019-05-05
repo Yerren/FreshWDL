364fb675fef3c4da0fba98c3e507ba6bc44c6f6f
Mark Hofmann <mark@weather-station.org>
2019-05-05 13:22:49 -0400

    Revert "Cosmetic Changes"
    This reverts commit 7dacb5e4e465a77d442ff696d09a390d383c4e2c.

--- config.js
+++ config.js
@@ -5,25 +5,25 @@ var clientRawName = "clientraw.txt", //The names of your clientraw files
     clientRawExtraName = "clientrawextra.txt",
     clientRawHourName = "clientrawhour.txt",
     clientRawDailyName = "clientrawdaily.txt",
-    customBaseURL = "http://www.weather-station.org/wdisplay/" // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
+    customBaseURL = false, // OPTIONAL: Set the path to where your clientraw files are uploaded e.g., "http://www.goldenbaynzweather.info/wdl/" (note: final backslash and quotation marks must be included). Otherwise leave as: false
     lang = "en",
     currentUnits = { //Default units (what the page will display when first loaded)
-        pressure: "inHg",        //Options: "hPa" "mmHG" "kPa" "inHg" "mb"
-        altitude: "ft",          //Options: "m" "yds" "ft"
-        wind: "mph",            //Options: "kmh" "mph" "kts" "ms" "mm" "inch" "B" (Beaufort)
-        rainfall: "inch",         //Options: "mm" "inch"
+        pressure: "hPa",        //Options: "hPa" "mmHG" "kPa" "inHg" "mb"
+        altitude: "m",          //Options: "m" "yds" "ft"
+        wind: "kmh",            //Options: "kmh" "mph" "kts" "ms" "mm" "inch" "B" (Beaufort)
+        rainfall: "mm",         //Options: "mm" "inch"
         windDirection: "deg",   //Options: "deg" (only one)
         humidity: "percent",    //Options: "percent" (only one)
         solar: "Wm",            //Options: "Wm" (only one)
         uv: "noUnit",           //Options: no units for UV
-        temp: "fahrenheit"         //Options: "celsius" "fahrenheit"
+        temp: "celsius"         //Options: "celsius" "fahrenheit"
     },
     gaugeSettings = { //Gauges: apparent temperature barometer windChill graphHandlerBarometer graphHandlerRainfall graphHandlerTemperature graphHandlerWindSpeed humidity moonSun solar status rainfallTitle rainfallDay rainfallMonth rainfallYear UV windDirection windSpeed 
         solar: {
-            enabled: false
+            enabled: true
         },
         UV: {
-            enabled: false
+            enabled: true
         },
         windChill: {
             mode: "windchill", //The default mode, either "heatIndex" or "windchill" (note: lowercase c)
