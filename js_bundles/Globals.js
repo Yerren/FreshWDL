76072794b6b5f7b9c42ac16607375e3a222ab005
Yerren <yerren@renerica.com>
2021-02-08 17:16:07 +1300

    Meteohub compadibility additional patch
    Fixed freezing issue

--- js_bundles/Globals.js
+++ js_bundles/Globals.js
@@ -2282,7 +2282,7 @@ function initialiseLayout() {
 	resizeContainer();
     
     //Set version number:
-    document.getElementById("Version").innerHTML = "FreshWDL - Version: 1.1.8.2 Alpha. yerren@renerica.com";
+    document.getElementById("Version").innerHTML = "FreshWDL - Version: 1.1.8.3 Alpha. yerren@renerica.com";
 }
 
 //Set global Graph options
