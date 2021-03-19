526db0997b128bf92bd6bde942f6d8e91857e18c
Yerren <yerren@renerica.com>
2021-03-20 11:10:18 +1300

    Testing jsdelivr

--- js_bundles/Globals.js
+++ js_bundles/Globals.js
@@ -2282,7 +2282,7 @@ function initialiseLayout() {
 	resizeContainer();
     
     //Set version number:
-    document.getElementById("Version").innerHTML = "FreshWDL - Version: 1.1.8.4 Alpha. yerren@renerica.com";
+    document.getElementById("Version").innerHTML = "FreshWDL - Version: 1.1.8.5 Alpha. yerren@renerica.com";
 }
 
 //Set global Graph options
